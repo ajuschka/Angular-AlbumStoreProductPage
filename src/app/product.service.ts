37d0ee01224dc7b43d07b96635790f23bf0dfca1
Juschka <ann-kristin.juschka@accenture.com>
2020-05-19 18:52:05 +0200

    used old import rxjs/add/operator/map as required for Pluralsight solution

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Http,Response } from '@angular/http'
 import { Observable } from 'rxjs/Observable';
-import { map } from 'rxjs/operators';
+//import { map } from 'rxjs/operators';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class ProductService {
@@ -11,6 +12,6 @@ export class ProductService {
   constructor(private _http : Http) { }
 
   getAlbum(id: number){
-    return this._http.get(this._albumUrl).pipe(map((response) => {return response.json()}));
+    return this._http.get(this._albumUrl).map((response) => {response.json()});
   }
 }
